9aa6a75a327ab6cc5c1ce2deb37c52563af95f9c
mohamdallam <mohamedahmedallam1@gmail.com>
2022-07-28 15:49:36 +0200

    init footer

--- src/components/8-footer/Footer.jsx
+++ src/components/8-footer/Footer.jsx
@@ -1,39 +1,87 @@
 import "./footer.css";
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 
-const footerLinks = [
+const footerQuickLinks = [
   { display: "Terms & Condition", url: "#" },
   { display: "Privacy Policy", url: "#" },
-  { display: "Terms & Condition", url: "#" },
+  { display: "Return & Refund", url: "#" },
   { display: "Payment", url: "#" },
+];
+
+const footerLinks = [
+  { display: "About", url: "#" },
+  { display: "Menu", url: "#" },
+  { display: "Recipes", url: "#" },
+  { display: "Contact", url: "#" },
 ];
 
 const Footer = () => {
   return (
-    <Container>
-      <Row>
-        <Col lg="4" md="4" sm="6">
-          <div className="logo">
-            <h2 className="d-flex align-item-center gap-1 mb-4">
-              <span>
-                <i class="ri-restaurant-2-line"></i>
-              </span>
-              Chef Food
-            </h2>
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="6">
+            <div className="logo">
+              <h2 className="d-flex align-item-center gap-1 mb-4">
+                <span>
+                  <i class="ri-restaurant-2-line"></i>
+                </span>
+                Chef Food
+              </h2>
 
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo,
-              omnis. Maxime eius fuga nihil harum aliquid ut aut
-            </p>
-          </div>
-        </Col>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo,
+                omnis. Maxime eius fuga nihil harum aliquid ut aut
+              </p>
+            </div>
+          </Col>
 
-        <Col lg="2" md="4" sm="6">
-          <h5 className="footer__link-title">Quick Links </h5>
-        </Col>
-      </Row>
-    </Container>
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__link-title">Info Link</h5>
+            <ListGroup>
+              {footerQuickLinks.map((item, index) => (
+                <ListGroupItem className="link__item" key={index}>
+                  <a href={item.url}>{item.display}</a>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+
+          <Col lg="2" md="4" sm="6">
+            <h5 className="footer__link-title">Quick Links </h5>
+            <ListGroup>
+              {footerLinks.map((item, index) => (
+                <ListGroupItem className="link__item" key={index}>
+                  <a href={item.url}>{item.display}</a>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__link-title">Contact</h5>
+            <ListGroup>
+              <ListGroupItem className="link__item">
+                <i class="ri-map-pin-line"></i> Egypt, Alexandria
+              </ListGroupItem>
+
+              <ListGroupItem className="link__item">
+                <i class="ri-mail-line"></i> example@gmail.com
+              </ListGroupItem>
+
+              <ListGroupItem className="link__item">
+                <i class="ri-phone-line"></i> 0123456789
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+
+      <div className="footer__bottom">
+        <p>copyright 2022 developed by Allam. All right reserved </p>
+      </div>
+    </div>
   );
 };
 
